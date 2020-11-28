d534c68458f5cc095228b1cade48e2206a63599b
miroshnykov <dmytro.miroshnykov@adsurge.com>
2020-11-28 09:57:47 -0500

    staging try catch

--- recipe/buildfiles.js
+++ recipe/buildfiles.js
@@ -19,42 +19,48 @@ const metrics = require('../lib/metrics')
 const createRecipeCampaign = async () => {
     // console.log('createfile Campaign')
     // console.time('createfileCampaign')
-    let campaignData = await campaigns()
+    try {
+        let campaignData = await campaigns()
 
-    let filePath = config.recipe.folder + await generateFilePath('campaign')
-    // console.log('filePath', filePath)
-    let fileFolder = path.dirname(filePath);
-    // console.log('fileFolder:', fileFolder)
-    await createRecursiveFolder(fileFolder)
-    // console.log('sfl_filePath:', filePath)
+        console.log('campaignData:', campaignData)
+        let filePath = config.recipe.folder + await generateFilePath('campaign')
+        // console.log('filePath', filePath)
+        let fileFolder = path.dirname(filePath);
+        // console.log('fileFolder:', fileFolder)
+        await createRecursiveFolder(fileFolder)
+        // console.log('sfl_filePath:', filePath)
 
-    // let sizeCampaign = await memorySizeOf(campaignData)
+        // let sizeCampaign = await memorySizeOf(campaignData)
 
-    // console.log('res.length', offerData.length)
-    // console.log('sizeOfDbMaps:', sizeOfDbMaps)
+        // console.log('res.length', offerData.length)
+        // console.log('sizeOfDbMaps:', sizeOfDbMaps)
 
-    let transformStream = JSONStream.stringify();
-    let outputStream = fileSystem.createWriteStream(filePath);
+        let transformStream = JSONStream.stringify();
+        let outputStream = fileSystem.createWriteStream(filePath);
 
-    transformStream.pipe(outputStream);
+        transformStream.pipe(outputStream);
 
-    campaignData.forEach(transformStream.write);
+        campaignData.forEach(transformStream.write);
 
-    transformStream.end();
+        transformStream.end();
 
-    outputStream.on(
-        "finish",
-        async function handleFinish() {
+        outputStream.on(
+            "finish",
+            async function handleFinish() {
 
-            // console.log(chalk.green("JSONStream serialization complete!"))
-            await compressFileZlibSfl(filePath)
-            await deleteFile(filePath)
-            // console.timeEnd('createfileCampaign')
-            // metrics.influxdb(200, `sizeOfCampaigns-${sizeCampaign}`)
-            console.log(`File Campaigns created path:${filePath}`)
+                // console.log(chalk.green("JSONStream serialization complete!"))
+                await compressFileZlibSfl(filePath)
+                await deleteFile(filePath)
+                // console.timeEnd('createfileCampaign')
+                // metrics.influxdb(200, `sizeOfCampaigns-${sizeCampaign}`)
+                console.log(`File Campaigns created path:${filePath}`)
 
-        }
-    );
+            }
+        );
+    } catch (e) {
+        metrics.influxdb(500, `createRecipeCampaignError'`)
+        console.log('createRecipeCampaignError:', e)
+    }
 
 
 }
@@ -62,41 +68,47 @@ const createRecipeOffers = async () => {
     // console.log('createfile with offers')
     // console.time('createFileOffers')
     // ************** CREATE ZIP FILE
-    let offerData = await offerInfo()
+    try {
+        let offerData = await offerInfo()
 
-    let filePath = config.recipe.folder + await generateFilePath('offer')
-    // console.log('filePath', filePath)
-    let fileFolder = path.dirname(filePath);
-    // console.log('fileFolder:', fileFolder)
-    await createRecursiveFolder(fileFolder)
-    // console.log('sfl_filePath:', filePath)
+        let filePath = config.recipe.folder + await generateFilePath('offer')
+        // console.log('filePath', filePath)
+        let fileFolder = path.dirname(filePath);
+        // console.log('fileFolder:', fileFolder)
+        await createRecursiveFolder(fileFolder)
+        // console.log('sfl_filePath:', filePath)
 
-    // let sizeOfOffers = await memorySizeOf(offerData)
+        // let sizeOfOffers = await memorySizeOf(offerData)
 
-    // console.log('res.length', offerData.length)
-    // console.log('sizeOfDbMaps:', sizeOfDbMaps)
+        // console.log('res.length', offerData.length)
+        // console.log('sizeOfDbMaps:', sizeOfDbMaps)
 
-    let transformStream = JSONStream.stringify();
-    let outputStream = fileSystem.createWriteStream(filePath);
+        let transformStream = JSONStream.stringify();
+        let outputStream = fileSystem.createWriteStream(filePath);
 
-    transformStream.pipe(outputStream);
+        transformStream.pipe(outputStream);
 
-    offerData.forEach(transformStream.write);
+        offerData.forEach(transformStream.write);
 
-    transformStream.end();
+        transformStream.end();
 
-    outputStream.on(
-        "finish",
-        async function handleFinish() {
+        outputStream.on(
+            "finish",
+            async function handleFinish() {
 
-            // console.log(chalk.green("JSONStream serialization complete!"));
-            await compressFileZlibSfl(filePath)
-            await deleteFile(filePath)
-            // console.timeEnd('createFileOffers')
-            // metrics.influxdb(200, `sizeOfOffers-${sizeOfOffers}`)
-            console.log(`File Offers created path:${filePath} `)
-        }
-    );
+                // console.log(chalk.green("JSONStream serialization complete!"));
+                await compressFileZlibSfl(filePath)
+                await deleteFile(filePath)
+                // console.timeEnd('createFileOffers')
+                // metrics.influxdb(200, `sizeOfOffers-${sizeOfOffers}`)
+                console.log(`File Offers created path:${filePath} `)
+            }
+        );
+    } catch {
+        metrics.influxdb(500, `createRecipeOffersError'`)
+        console.log('createRecipeOffersError:', e)
+    }
+
 
 }
 
