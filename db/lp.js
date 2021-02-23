36064389fec258ec12c837a5cca1a2f55dfab28d
miroshnykov <dmytro.miroshnykov@adsurge.com>
2021-02-23 16:26:44 -0500

    lp sql  format

--- db/lp.js
+++ db/lp.js
@@ -5,12 +5,12 @@ const getLps = async () => {
     try {
         let result = await dbMysql.query(` 
             SELECT  
-                lp.sfl_segment_id AS segmentId, 
-                l.name AS name, 
-                lp.weight AS weight,
-                l.forced_landing_url AS forcedLandingUrl,
-                l.static_url AS staticUrl,
-                l.product_id AS productId
+                lp.sfl_segment_id       AS segmentId, 
+                l.name                  AS name, 
+                lp.weight               AS weight,
+                l.forced_landing_url    AS forcedLandingUrl,
+                l.static_url            AS staticUrl,
+                l.product_id            AS productId
             FROM landing_pages l, sfl_segment_landing_page lp
             WHERE l.id = lp.landing_pages_id
             ORDER BY 1
